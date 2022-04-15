2e17522a2101e42c8a3f8439c76e5bd836deca8b
katahalonen <kata.halonen@gmail.com>
2022-04-15 13:39:45 +0300

    Initialization
    First commit containing character controller.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    // Message displayed to player when looking at an interactable.
+    public string promptMessage;
+
+}
